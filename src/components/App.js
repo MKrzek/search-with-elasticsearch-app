f344d0eb9faa87690be4ab301043c2d343c62f41
Malgorzata <mkrzek@googlemail.com>
2018-01-14 18:31:52 +0000

    added a search bar

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+import SearchBar from './SearchBar.js'
+class App extends Component {
+  render() {
+    return <SearchBar/>
+      
+    
+  }
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {Field, reduxForm} from 'redux-form';
+import {connect} from 'react-redux';
+import * as Actions from '../actions/index.js';
+
+class SearchBar extends React.Component{
+renderSearchBar=(field)=>{
+    console.log ('field.meta.error', field.meta.error)
+    
+    return <fieldset className='form-group'>
+                <label className='col form-label'>{field.label}</label>
+                <input {...field.input} type={field.type}/>
+                <div>{field.meta.touched ? field.meta.error : ''}</div>
+           </fieldset>
+}
+submitSearchTerm=(values)=>{
+    console.log('values', values)
+    this.props.performQuery(values)
+}
+    render(){
+        const{handleSubmit}=this.props
+        return<div className='container'>
+               <form onSubmit={handleSubmit(this.submitSearchTerm)}>
+               <Field
+               name='searchBar'
+               label='enter your query'
+               component={this.renderSearchBar}
+               type='text'
+              />
+              <button type='submit' className='btn btn-primary'>Search</button>
+              </form>
+              </div>
+            
+    };
+};
+function validate(values){
+    const errors={};
+    if (!values.SearchBar){
+        errors.SearchBar='Please enter your query'
+    }
+    console.log ('errors', errors)
+    return errors
+};
+
+export default connect(null, Actions)(reduxForm ({
+ form: 'searchBar', 
+ validate
+})(SearchBar))
